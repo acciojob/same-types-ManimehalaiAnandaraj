df8145657a527a63a0b47d3db66ec8168f04a784
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-06 14:45:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,12 @@ function isSameType(value1, value2) {
   //your js code here
 if(typeof(value1) === typeof(value2) 
 {
-	else if (isNaN(value1) && isNaN(value2)){
+	 if (isNaN(value1) && isNaN(value2)){
 return true;
 }
-	else if (isNaN(value1) || isNaN(value2)){
+}	else if (isNaN(value1) || isNaN(value2)){
 		return false;
-	}
-return true;
-	
-}
-	return false;}
+	}}
 
 
 
