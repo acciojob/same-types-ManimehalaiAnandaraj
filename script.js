5c8720f8af9d9d127152603812613e1e1e460505
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-06 13:40:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function isSameType(value1, value2) {
   //your js code here
-if(typeof(value1) === typeof(value2))
+if(isSameType(value1) === isSameType(value2))
+{return true;}
 
-return true;
 
-else
+
 
 	return false;
 
