18ebe9c1c0570d419eb49bd135fb3d7c0871813c
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-06 14:52:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ if(typeof(value1) === typeof(value2)
 	 if (isNaN(value1) && isNaN(value2)){
 return true;
 }
-}	else if (isNaN(value1) || isNaN(value2)){
+	else if (isNaN(value1) || isNaN(value2)){
 		return false;
-	}}
+	}
+return true;
+}
+	return false;
+}
 
 
 
